4650de4cba274344ae6c0cf825a3c07588ab3390
Brian Allred <brian@allred.io>
2025-01-10 18:33:33 -0600

    Make sure config files only live in one place on Linux

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs';
+import * as os from 'os';
+import xdg from 'xdg-portable';
 
 export default class Config {
   private static CONFIG_FILE = 'launcher-config.json';
@@ -8,12 +10,12 @@ export default class Config {
   sdcardPath: string = '';
 
   private static createFile() {
-    if (!fs.existsSync(Config.CONFIG_FILE)) {
+    if (!fs.existsSync(Config.configFilePath())) {
       const configStr = JSON.stringify({
         ryuPath: null,
         sdcardPath: null,
       });
-      fs.writeFileSync(Config.CONFIG_FILE, configStr);
+      fs.writeFileSync(Config.configFilePath(), configStr);
     }
   }
 
@@ -22,11 +24,11 @@ export default class Config {
     Config.createFile();
 
     // read the file
-    return JSON.parse(fs.readFileSync(Config.CONFIG_FILE, 'utf-8')) as Config;
+    return JSON.parse(fs.readFileSync(Config.configFilePath(), 'utf-8')) as Config;
   }
 
   private static saveConfig(config: Config) {
-    fs.writeFileSync(Config.CONFIG_FILE, JSON.stringify(config));
+    fs.writeFileSync(Config.configFilePath(), JSON.stringify(config));
   }
 
   static getRyuPath() {
@@ -49,6 +51,16 @@ export default class Config {
     const config = Config.readFile();
     config.sdcardPath = path;
     Config.saveConfig(config);
+  }
+
+  // Ensure that the config file only lives in one place on linux
+  // Path is `/home/user/.config/hdr-launcher/launcher-config.json`
+  private static configFilePath() {
+    if (os.platform() == 'linux') {
+      return xdg.config() + '/hdr-launcher/' + this.CONFIG_FILE;
+    }
+
+    return this.CONFIG_FILE;
   }
 
   private constructor() {}
